bbfc88a1ba00d6ea885593644b7ad5b60314d77d
lambertco <blambertco@gmail.com>
2019-11-06 16:06:34 -0700

    added and configured firebase and started building table

---
+++ src/components/Firebase/Firebase.js
@@ -0,0 +1,35 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDUzI4kXKEpWapGiZ0f6nN0d0JQ7mrb9WU',
+    authDomain: 'tank-temps.firebaseapp.com',
+    databaseURL: 'https://tank-temps.firebaseio.com',
+    projectId: 'tank-temps',
+    storageBucket: "",
+    messagingSenderId: '355631466975',
+    appId: '1:355631466975:web:4bd593965c255922795c6a',
+    measurementId: 'G-V89HNETHYP'
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig);
+
+        this.auth = app.auth();
+    }
+    // create user
+    doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+    // sign in
+    doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+    //sign out 
+    doSignOut = () => this.auth.signOut();
+    //password reset
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
