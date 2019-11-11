202a8a68e0b1807ef5097dda73e392cd05791896
Jlynnraz <jlynnraz@gmail.com>
2019-11-11 10:11:35 -0700

    commiting again

--- src/components/Tanks/Tanks.js
+++ src/components/Tanks/Tanks.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import tanks from "../../tanks.json";
 import './Tanks.css';
+import firebase from "firebase";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 
 class Tank extends Component {
@@ -9,9 +11,14 @@ class Tank extends Component {
         tanks
     }
 
+    
     render() {
+     const handleSignOut = () => {
+            firebase.auth().signOut()
+          }
         return (
            <>
+           <button onClick={handleSignOut}>Sign Out</button>
            <div className="col-md-6">
                <table>
                    <thead>
