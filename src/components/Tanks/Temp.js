8b83ddf873970ff67cf6506490c786516c9a6e0a
Jlynnraz <jlynnraz@gmail.com>
2019-12-09 16:26:00 -0700

    wired up form on temp page

--- src/components/Tanks/Temp.js
+++ src/components/Tanks/Temp.js
@@ -3,62 +3,60 @@ import styled from 'styled-components';
 import * as firebase from 'firebase';
 
 
-class Temp extends Component {
+class Temp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
-    state = {
-      setTemp: ""
-    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    onChange =  (event) => {
-      const { setTemp } = event.target;
-    };
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
-    onSubmit = (event) => {
-      event.preventDefault()
-      const { setTemp } = this.state;
-    };
+  handleSubmit(event) {
+    alert('A temp was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
 
   render() {
 
-    const { setTemp } = this.state;
-
     const handleSignOut = () => {
-      firebase.auth().signOut()
-  }
-   return (
-  <>
-    <div className="col-sm-2">
-    <SignOut onClick={handleSignOut} type="submit"  className="btn btn">Sign Out</SignOut>
-    <Tanks onClick={<Route exact path='/tank'/>} type="submit"  className="btn btn">Tanks</Tanks>
-    </div>
+       firebase.auth().signOut()
+    }
 
-      <Form>
-      <div className="form-row">
-     
+    return (
+      <>
+      <div className="col-sm-2">
+      <SignOut onClick={handleSignOut} type="submit"  className="btn btn">Sign Out</SignOut>
+      <Tanks onClick={<Route exact path='/tank'/>} type="submit"  className="btn btn">Tanks</Tanks>
+      </div>
+
+      
+      <Form onSubmit={this.handleSubmit}>
+        <div className="form-row">
       <div className="col-md-2 offset-md-5 align-items-start">
-      <p>Set target temp here:</p>
-      <input  
-              type="text"
-              name="setTemp"
-              value={this.setTemp}
-              onChange={this.onChange}
-              placeholder="Target Temp"/>
-              
-      </div>
-      <div className="col-md-10 offset-sm-6 align-items-center">
-      <Submit onClick={this.onSubmit} type="submit" className="btn sm">Submit</Submit>
-      </div>
-
-      </div>
+        <label>
+         <p>Set target temp here:</p>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        </div>
+        <div className="col-md-10 offset-sm-6 align-items-center">
+        <input type="submit" value="Submit" />
+        </div>
+        </div>
       </Form>
-   </>  
-   )
-   
-  };
 
-};
+      </>
+    );
+  }
+}
 
 export default Temp
+
+
 
 
 const Submit = styled.button `
